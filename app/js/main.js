8f3ce321c7129635f3551810a36a47def558dd4a
reegterz <amironenko72@gmail.com>
2022-03-04 12:09:16 +0500

    create detalis

--- app/js/main.js
+++ app/js/main.js
@@ -5,15 +5,34 @@ $(function(){
         fade: true,
         autoplay: true,
         autoplaySpeed: 2000
-    })
+    });
+    $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false
+    });
+    $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade: true
+    });
+    
+
 });
+$(function(){
+    $('.product-one__item-num').styler();
+});
+
 $(function(){
     $('.filter-category__checkbox').on('click',function(){
         $('.products-content').toggleClass('products-content--list');
     });
     
 });
-
 
 $(function(){
     $('.filter-price__input').ionRangeSlider({
@@ -32,13 +51,20 @@ $(function(){
         starWidth: "11px",
         normalFill: "#d6d6d6",
         ratedFill: "#ffcc00",
-        readOnly: true
+        readOnly: true,
     });
     $('.products-content__star').rateYo({
         starWidth: "17px",
         normalFill: "#d6d6d6",
         ratedFill: "#ffcc00",
-        readOnly: true
+        readOnly: true,
+    });
+    $('.product-one__star').rateYo({
+        starWidth: "17px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true,
     })
+
 
 });
